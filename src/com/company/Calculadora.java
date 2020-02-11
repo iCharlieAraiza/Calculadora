0d34b848fec32429c84434dd19c3aad6ca2096c1
Carlos Araiza <caraiza_10@hotmail.com>
2020-02-11 16:00:57 -0600

    Initial commit

---
+++ src/com/company/Calculadora.java
@@ -0,0 +1,45 @@
+package com.company;
+import java.util.Scanner;
+
+public class Calculadora {
+    double numero;
+
+    public void setNumero( double numero){
+        this.numero = numero;
+    }
+
+    public double getNumero(){
+        return numero;
+    }
+
+    public double suma( double n){
+        return this.numero = numero + n;
+    }
+
+    public double resta (double n){
+        return this.numero = numero - n;
+    }
+
+    public double multiplicacion(double n){
+        return this.numero = numero * n;
+    }
+
+    public double division(double n){
+        Scanner Input = new Scanner( System.in );
+        if(n==0){
+            do{
+                //scannear numero
+                System.out.println("¡Ups! No se puede dividir entre 0. Agrega otro valor");
+                System.out.print("Número: ");
+                n = Double.parseDouble( Input.nextLine() );
+
+            }while(n==0);
+        }
+        return this.numero = numero / n;
+    }
+
+    static public boolean checkOp(String op){
+        return ( op.equals("+") || op.equals("-") || op.equals("*") || op.equals("/") || op.equals("s")  ) ? true : false;
+    }
+
+}
---
+++ src/com/company/Main.java
@@ -0,0 +1,65 @@
+package com.company;
+
+import java.util.Scanner;
+
+public class Main {
+
+    public static void main(String[] args) {
+	// write your code here
+        boolean s = true, op_c = false;
+        String num, op;
+        char opc;
+        double r;
+
+
+        Calculadora Cal = new Calculadora();
+        Scanner Input = new Scanner( System.in );
+
+        System.out.println("Instrucciones.\nPara salir del programa, solo debes agregar la letra 's' en el campo de 'Operador' ");
+        System.out.println("*************************************************************************************************");
+
+        System.out.print("Número: ");
+        num = Input.nextLine();
+
+        Cal.setNumero( Double.parseDouble(num) );
+
+        do{
+
+            do{
+                System.out.print("Operador (+, -, *, /): ");
+                op = Input.nextLine();
+                if( !Calculadora.checkOp(op) ){
+                    System.out.println("Has agregado una operación no válida");
+                }
+            }while( !Calculadora.checkOp(op) );
+
+            if( op.equals("s")){
+                s=false;
+            }else{
+                System.out.print("Número: ");
+                num = Input.nextLine();
+                operaciones(Cal, Double.parseDouble(num), op);
+            }
+
+        }while(s);
+    }
+
+    static public void operaciones(Calculadora Cal, double num, String op){
+        switch(op.charAt(0)){
+            case '+':
+                Cal.suma(num);
+                break;
+            case '-':
+                Cal.resta(num);
+                break;
+            case '/':
+                Cal.division(num);
+                break;
+            case '*':
+                Cal.multiplicacion(num);
+                break;
+        }
+        System.out.println( Cal.getNumero() );
+    }
+
+}
